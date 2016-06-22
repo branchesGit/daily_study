be8c7de5a3a856d6263ddeaa06364959868f7344
branchesGit <3085038458@qq.com>
2016-06-22 15:12:12 +0800

    update gulp server to auto

--- react_s/gulpfile.js
+++ react_s/gulpfile.js
@@ -5,30 +5,36 @@ var rename = require('gulp-rename');
 var del = require('del');
 var webpack = require('gulp-webpack');
 var config = require('./webpack.config');
-var connect = require("gulp-connect");
+
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
+
 
 gulp.task('clean', function(){
 	del(['build/']);
 });
 
-gulp.task('server', ['js'], function(){
-	 browserSync.init({
+gulp.task('server', ['pack_js'], function(){
+	browserSync.init({
 		server: {
 			baseDir: "./"
-			}
-    });
+		}
+	});
 
-    gulp.watch("*.html").on("change", reload);
-    gulp.watch('./src/*/*.js', ['js-watch']);
+	//修改html时，加载页面
+	gulp.watch("*.html").on("change", reload);
+
+	gulp.watch('./src/**/*.js', ['reload_by_js']);
 });
 
-gulp.task('js', ['clean'], function() {
-  gulp.src('./src')
-  	.pipe(webpack(config))
-  	.pipe(uglify())
-  	.pipe(gulp.dest("build/"));
+gulp.task('pack_js', ['clean'], function(){
+	gulp.src('./src')
+	.pipe(webpack(config))
+	.pipe(uglify())
+	.pipe(gulp.dest("build/"))
+	.pipe(reload({stream:true})); //在输出流里强行注入
 });
 
-gulp.task('js-watch', ['js'], reload);
+//执行顺序并不对
+gulp.task('reload_by_js', ['pack_js'], reload);
+
